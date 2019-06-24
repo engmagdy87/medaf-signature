8a3fb568b67590b26bb5ab092057321687c9dacd
engmagdy87 <eng.magdy87@gmail.com>
2019-06-24 03:48:52 +0200

    first commit

---
+++ data.js
@@ -0,0 +1,276 @@
+const data = [
+  {
+    "firstName": "Adnan",
+    "lastName": "Al Salem",
+    "email": "adnan.alsalem@medafinvestment.com",
+    "mobile": "+96522923399  / '01010488888",
+    "title": "Chairman",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Ahmed",
+    "lastName": "Hindy",
+    "email": "ahmed.hindy@medafinvestment.com",
+    "mobile": "(+20) 1222164545",
+    "title": "Deputy Managing Director",
+    "company": "Medaf Pay"
+  },
+  {
+    "firstName": "Ahmed",
+    "lastName": "El Hamahmy",
+    "email": "ahmed.elhamahmy@medafinvestment.com",
+    "mobile": "(+20) 1222171582",
+    "title": "Head of Finance",
+    "company": "Medaf Finance Holding"
+  },
+  {
+    "firstName": "Ahmed",
+    "lastName": "El Aroussi",
+    "email": "ahmed.elaroussi@medafinvestment.com",
+    "mobile": "(+20) 1223304754",
+    "title": "Assistant Managing Director for Project Management",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Ahmed",
+    "lastName": "Elrawy",
+    "email": "ahmed.elrawy@medafinvestment.com",
+    "mobile": "(+20) 1022228090",
+    "title": "Managing Director",
+    "company": "Medaf Securities"
+  },
+  {
+    "firstName": "Ahmed",
+    "lastName": "Neyazy",
+    "email": "ahmed.neyazy@medafinvestment.com",
+    "mobile": "(+20) 1005874903",
+    "title": "Financial Controller",
+    "company": "Medaf Finance Holding"
+  },
+  {
+    "firstName": "Amr",
+    "lastName": "Zaghloul",
+    "email": "amr.zaghloul@medafinvestment.com",
+    "mobile": "(+20) 1001931010",
+    "title": "Chairman",
+    "company": "Medaf Securities"
+  },
+  {
+    "firstName": "Azza",
+    "lastName": "Saleh, CFA, CPA",
+    "email": "azza.saleh@medafinvestment.com",
+    "mobile": "(+20) 1001021184",
+    "title": "Head of Investments",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Ehab",
+    "lastName": "El Beshbishy",
+    "email": "ehab.elbeshbishy@medafinvestment.com",
+    "mobile": "(+20) 1000089560",
+    "title": "Operation Manager for Deals & Listings",
+    "company": "Medaf Securities"
+  },
+  {
+    "firstName": "Hamdy",
+    "lastName": "Rashad",
+    "email": "hamdy.rashad@medafinvestment.com",
+    "mobile": "(+20) 1222101681",
+    "title": "Chairman",
+    "company": "Medaf Finance Holding"
+  },
+  {
+    "firstName": "Hanan",
+    "lastName": "Wagdy",
+    "email": "hanan.wagdy@medafinvestment.com",
+    "mobile": "(+20) 1005355590",
+    "title": "Assistant Managing Director for Financial Institutions",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Hany",
+    "lastName": "Abou El Fotouh",
+    "email": "hany.abouelfotouh@medafinvestment.com",
+    "mobile": "(+20) 1066614888",
+    "title": "Head of Governance & Compliance",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Hatem",
+    "lastName": "El Alfy",
+    "email": "hatem.elalfy@medafinvestment.com",
+    "mobile": "(+20) 1001676414",
+    "title": "Investment Manager",
+    "company": "Medaf Securities"
+  },
+  {
+    "firstName": "Iten",
+    "lastName": "El-Orabi",
+    "email": "iten.elorabi@medafinvestment.com",
+    "mobile": "",
+    "title": "",
+    "company": ""
+  },
+  {
+    "firstName": "Jasem",
+    "lastName": "Al Faji",
+    "email": "jasem.alfaji@medafinvestment.com",
+    "mobile": "(+96) 565676661 / (+96) 522923399",
+    "title": "Founding Partner / Vice Chairman",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Khaled",
+    "lastName": "Abd ElKader",
+    "email": "khaled.elkader@medafinvestment.com",
+    "mobile": "(+20) 1222157290",
+    "title": "Head of Risk",
+    "company": "Medaf Finance Holding"
+  },
+  {
+    "firstName": "Lucy",
+    "lastName": "Iskander",
+    "email": "lucy.iskander@medafinvestment.com",
+    "mobile": "(+20) 1222130213",
+    "title": "Managing Director",
+    "company": "Medaf Leasing & Factoring"
+  },
+  {
+    "firstName": "Maher",
+    "lastName": "Magdy",
+    "email": "maher.magdy@medafinvestment.com",
+    "mobile": "(+20) 1225467614",
+    "title": "Head of Technology",
+    "company": "Medaf Pay"
+  },
+  {
+    "firstName": "Mohab",
+    "lastName": "Zakaria",
+    "email": "mohab.zakaria@medafinvestment.com",
+    "mobile": "(+20) 1001433362",
+    "title": "Vice Chairman/ Chief Executive Officer",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Mohamed",
+    "lastName": "Hassan",
+    "email": "mohamed.hassan@medafinvestment.com",
+    "mobile": "(+20) 1005604239",
+    "title": "Managing Director",
+    "company": "Medaf Asset Management"
+  },
+  {
+    "firstName": "Mohammed",
+    "lastName": "Alaa",
+    "email": "mohammed.alaa@medafinvestment.com",
+    "mobile": "(+20) 1000605922",
+    "title": "Partner / Board Member",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Mostafa",
+    "lastName": "Kamal",
+    "email": "mostafa.kamal@medafinvestment.com",
+    "mobile": "(+20) 1223215422",
+    "title": "Head of Document Control",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Nouran",
+    "lastName": "Khaled",
+    "email": "nouran.khaled@medafinvestment.com",
+    "mobile": "(+20) 1000344221",
+    "title": "Payroll Specialist",
+    "company": "Medaf Finance Holding"
+  },
+  {
+    "firstName": "Omar",
+    "lastName": "Mostafa",
+    "email": "omar.mostafa@medafinvestment.com",
+    "mobile": "(+20) 1000722412",
+    "title": "Financial Analyst",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Omar",
+    "lastName": "Ezzat",
+    "email": "omar.ezzat@medafinvestment.com",
+    "mobile": "(+20) 1069045546",
+    "title": "Financial Analyst",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Rawan",
+    "lastName": "Shetaa",
+    "email": "rawan.shetaa@medafinvestment.com",
+    "mobile": "(+20) 1125333157",
+    "title": "Document Control Specialist",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Samira",
+    "lastName": "Noureldin",
+    "email": "samira.noureldin@medafinvestment.com",
+    "mobile": "(+20) 1006969269",
+    "title": "Human Resource Manager",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Victor",
+    "lastName": "Antoun",
+    "email": "victor.antoun@medafinvestment.com",
+    "mobile": "(+20) 125783535",
+    "title": "Manager, IT Networks",
+    "company": "Medaf Pay"
+  },
+  {
+    "firstName": "Yaser",
+    "lastName": "Agiba",
+    "email": "yaser.agiba@medafinvestment.com",
+    "mobile": "(+20) 1017781500",
+    "title": "Partner / Board Member",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Mohammed M",
+    "lastName": "Gad",
+    "email": "mohammed.gad@medafinvestment.com",
+    "mobile": "(+20) 1222169980",
+    "title": "Chairman",
+    "company": "Medaf Asset Management"
+  },
+  {
+    "firstName": "Dr. Ahmed",
+    "lastName": "Darwish",
+    "email": "ahmed.darwish@medafinvestment.com",
+    "mobile": "(+2) 02-24619581",
+    "title": "Board Member",
+    "company": "Medaf Finance Holding"
+  },
+  {
+    "firstName": "Gen. Ahmed",
+    "lastName": "El Gohary",
+    "email": "ahmed.elgohary@medafinvestment.com",
+    "mobile": "(+20) 1006033458",
+    "title": "Board Member",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Gen. Alaa",
+    "lastName": "Fathy",
+    "email": "alaa.fathy@medafinvestment.com",
+    "mobile": "(+20) 1142226660",
+    "title": "Assistant Managing Director for Government Relations",
+    "company": "Medaf Investments"
+  },
+  {
+    "firstName": "Alaa",
+    "lastName": "Elrawy",
+    "email": "alaa.elrawy@medafinvestment.com",
+    "mobile": "(+20) 1001446278",
+    "title": "Deputy Managing Director",
+    "company": "Medaf Pay"
+  }
+]
+
+export default data
